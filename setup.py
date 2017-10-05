6e21f32b454a17ca01a9c0dd37ac48768b1ec33d
Bo Peng <ben.bog@gmail.com>
2017-10-04 22:58:02 -0500

    Add setup.py to sos-all

---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+#
+# This file is part of Script of Scripts (sos), a workflow system
+# for the execution of commands and scripts in different languages.
+# Please visit https://github.com/vatlab/SOS for more information.
+#
+# Copyright (C) 2016 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from setuptools import setup
+
+__version__ = '0.9.9.1'
+
+description = '''\
+Computationally intensive disciplines such as computational biology often 
+requires one to exploit a variety of tools implemented in different programming
+languages, and to analyze large datasets on high performance computing systems.
+Although scientific workflow systems are powerful in organizing and executing
+large-scale data analysis processes, there are usually non-trivial learning
+curve and engineering overhead in creating and maintaining such workflows,
+making them unsuitable for data exploration and prototyping. To bridge the
+gap between interactive analysis and workflow systems, we developed Script
+of Scripts (SoS), a system with strong emphases on readability, practicality,
+and reproducibility for daily computational research. For exploratory analysis
+SoS provides a multi-language file format and scripting engine that centralizes
+all computations, and creates dynamic report documents for publishing and
+sharing. As a workflow engine, SoS provides an intuitive syntax to create
+workflows in process-oriented, outcome-oriented and mixed styles, as well as
+a unified interface to executing and managing tasks on a variety of computing
+platforms with automatic synchronization of files between isolated systems.
+In this paper we illustrate with real-world examples the use of SoS as both
+interactive analysis tool and pipeline platform for all stages of methods
+development and data analysis projects. In particular we demonstrate how SoS
+can easily be adopted based on existing scripts and pipelines, yet resulting
+in substantial improvement in terms of organization, readability and
+cross-platform computation management.
+
+Please refer to http://vatlab.github.io/SOS/ for more details on SoS.
+'''
+
+#
+# This is a super package for sos with the sole purpose to install
+# components of SoS with a single command.
+#
+
+setup(name = "sos-all",
+    version = __version__,
+    description = '''Script of Scripts (SoS): an interactive, cross-platform,
+        and cross-language workflow system for reproducible data analysis''',
+    long_description=description,
+    author = 'Bo Peng',
+    url = 'https://github.com/vatlab/SOS',
+    author_email = 'bpeng@mdanderson.org',
+    maintainer = 'Bo Peng',
+    maintainer_email = 'bpeng@mdanderson.org',
+    license = 'GPL3',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    install_requires=[
+        'sos',
+        'sos-notebook',
+        'sos-pbs',
+        'sos-rq',
+        'sos-bash'
+        'sos-javascript',
+        'sos-julia',
+        'sos-python',
+        'sos-r',
+        'sos-sas',
+        'sos-matlab',
+        'sos-bioinfo',
+      ]
+)
